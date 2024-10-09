b045d7546453f184905392ca67f10761dab47cfd
João Ribeiro <joao.ribeiro@controlink.pt>
2024-10-09 21:18:42 +0100

    🔧 Update Easypay Facade: Add config() and payByLink() methods for easier access to related controllers and services.

--- src/Facades/Easypay.php
+++ src/Facades/Easypay.php
@@ -7,12 +7,12 @@ use Controlink\LaravelEasypay\Services\EasypayPayByLinkService;
 use Controlink\LaravelEasypay\Services\EasypayService;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Controlink\LaravelEasypay\Http\Controllers\EasypayConfigurationController config()
+ * @method static \Controlink\LaravelEasypay\Services\EasypayPayByLinkService payByLink()
+ */
 class Easypay extends Facade
 {
-    /**
-     * @method static \Controlink\LaravelEasypay\Http\Controllers\EasypayConfigurationController config()
-     * @method static \Controlink\LaravelEasypay\Services\EasypayPayByLinkService payByLink()
-     */
     protected static function getFacadeAccessor()
     {
         return 'easypay';
